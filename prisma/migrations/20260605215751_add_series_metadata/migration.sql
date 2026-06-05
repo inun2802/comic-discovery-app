49512b283afb3ad08d4f28c9b1767ef7627d1a88
inun2802 <isaiahnunez1028@gmail.com>
2026-06-05 15:04:11 -0700

    Batman Issues added, Universes retitled and added, new paramaters in series.json and schema created

---
+++ prisma/migrations/20260605215751_add_series_metadata/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Series" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "endYear" INTEGER,
+ADD COLUMN     "issueCount" INTEGER,
+ADD COLUMN     "startYear" INTEGER,
+ADD COLUMN     "universe" TEXT;
